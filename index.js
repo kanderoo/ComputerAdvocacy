329eef1339d7f6aec27c2c16418b829b13d66739
Andrew <kandrew2022@gmail.com>
2019-12-03 15:54:56 -0500

    Added test input
    Added a question/answer in index.html and verification based on answerArray in index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function test() {
-	alert("test"); 
+function submit() {
+	let answerArray = ["no"]
+	for (let i = 0; i < answerArray.length; i++) {
+		if (document.getElementsByTagName("input")[i].value == answerArray[i]) {
+			alert("yay");
+		}
+	}
 }
