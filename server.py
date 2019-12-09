4d25cee21f0344eaee42f8a154daca777779ac46
Inam Kamali <inam@garfielf.net>
2019-12-08 19:20:22 -0500

    Added some gui stuff

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
+import tkinter as tk 
 # hello_world.py
 from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 
+data = []
+name = []
+
+tk = tk.Tk()
+tk.title('Leaderboard')
+tk.resizable(0,0)
+leaderboard = Label(tk, text="boop")
+leaderboard.pack()
+tk.update()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def handleData():
+    pass
+
+
 
 if __name__ == '__main__': app.run(host='0.0.0.0',port='8080')
