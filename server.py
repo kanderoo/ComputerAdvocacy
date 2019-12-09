c863842fc8ec9758cd66049ca13a5294f766ae19
Andrew <kandrew2022@gmail.com>
2019-12-08 19:17:20 -0500

    Added Name query and sends POST to server
    Added a field for the user's name and wrote a function in index.js to submit a POST request to the server with the user's name and time. The server currently prints it out in the console.

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 # hello_world.py
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+	print(request.json)
+	return render_template('index.html')
 
 if __name__ == '__main__': app.run(host='0.0.0.0',port='8080')
